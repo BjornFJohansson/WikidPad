5729942d3763500b8daf85e80008c9d5b3e678c9
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-11-12 07:16:45 +0000

    stuf for shortcuts

---
+++ create_shortcut.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pprint import pprint
+import sys
+
+import pyshortcuts.utils
+
+from .paths import HERE, ICON_PATH
+
+
+def make_shortcuts():
+    script = str(HERE / "launch.py")
+    name = "RobotLab"
+    print("Making RobotLab shortcut in $HOME...")
+    print("", pyshortcuts.utils.get_homedir())
+    scut = pyshortcuts.make_shortcut(
+        script=script,
+        name=name,
+        terminal=True,
+        description="Launch RobotLab in Firefox in your $HOME",
+        icon=ICON_PATH,
+    )
+    ""
+    print("Shortcut created...")
+    pprint(scut.__dict__)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(make_shortcuts())
